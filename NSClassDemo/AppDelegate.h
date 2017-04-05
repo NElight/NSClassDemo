9c6104fdde175144ef59b4223d6bf25f9bae8116
Mac <gg392811726@icloud.com>
2017-04-05 20:05:50 +0800

    init
    init

---
+++ NSClassDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSClassDemo
+//
+//  Created by Mac on 17/4/5.
+//  Copyright © 2017年 light. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSClassDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSClassDemo
+//
+//  Created by Mac on 17/4/5.
+//  Copyright © 2017年 light. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
